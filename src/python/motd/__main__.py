896cc15bd7f49b2c5ca186fbd685153449aad0e8
Robin Trietsch <robin.trietsch@gmail.com>
2020-05-12 09:15:27 +0200

    motd plugin now using relative date in timeline

--- src/python/motd/__main__.py
+++ src/python/motd/__main__.py
@@ -1,5 +1,6 @@
 import requests
 from ics import Calendar
+from arrow import Arrow
 
 from .config import MotdConfig
 from .domain import MotdIcons
@@ -10,7 +11,9 @@ if __name__ == '__main__':
 
         cal = Calendar(req.text)
 
-        today = list(cal.timeline)[0]
+        # Shift one day, as the timeline has an offset of one day
+        now_plus_one_day = Arrow.now().shift(days=1)
+        today = list(cal.timeline.on(now_plus_one_day))[0]
         motd_first_name = list(today.attendees)[0].common_name.split(' ')[0]
 
         print(f'MotD: {motd_first_name} | templateImage={MotdIcons.MOTD_LOGO.base64_image}')
