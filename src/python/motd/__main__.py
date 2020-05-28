d8fa8d3946208e009bcf3fd00328faf360dd82de
Robin Trietsch <robin.trietsch@gmail.com>
2020-05-28 08:59:53 +0200

    Better exception handling motd

--- src/python/motd/__main__.py
+++ src/python/motd/__main__.py
@@ -7,15 +7,12 @@ from .domain import MotdIcons
 
 
 def print_current_motd(_calendar: Calendar):
-    try:
-        # Shift one day, as the timeline has an offset of one day
-        now_plus_one_day = Arrow.now().shift(days=1)
-        today = list(_calendar.timeline.on(now_plus_one_day))[0]
-        motd_first_name = list(today.attendees)[0].common_name.split(' ')[0]
+    # Shift one day, as the timeline has an offset of one day
+    now_plus_one_day = Arrow.now().shift(days=1)
+    today = list(_calendar.timeline.on(now_plus_one_day))[0]
+    motd_first_name = list(today.attendees)[0].common_name.split(' ')[0]
 
-        print(f'MotD: {motd_first_name} | templateImage={MotdIcons.MOTD_LOGO.base64_image}')
-    except:
-        print(f'MotD: ? | templateImage={MotdIcons.MOTD_LOGO.base64_image}')
+    print(f'MotD: {motd_first_name} | templateImage={MotdIcons.MOTD_LOGO.base64_image}')
 
 
 def print_coming_motd(_calendar: Calendar):
@@ -39,9 +36,12 @@ def print_coming_motd(_calendar: Calendar):
 
 
 if __name__ == '__main__':
-    req = requests.get(MotdConfig.MOTD_ICAL_URL)
+    try:
+        req = requests.get(MotdConfig.MOTD_ICAL_URL, timeout=5)
 
-    calendar = Calendar(req.text)
+        calendar = Calendar(req.text)
 
-    print_current_motd(calendar)
-    print_coming_motd(calendar)
+        print_current_motd(calendar)
+        print_coming_motd(calendar)
+    except:
+        print(f'MotD: ? | templateImage={MotdIcons.MOTD_LOGO.base64_image}')
